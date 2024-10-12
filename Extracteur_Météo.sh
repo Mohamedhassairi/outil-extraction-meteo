007928cdaf82283093b8b3f1b6daacfec7d53069
ASSEF <mhtzeneassef@gmail.com>
2024-10-13 01:34:11 +0200

    Mise à jour du script pour gérer les villes inexistantes

--- Extracteur_Météo.sh
+++ Extracteur_Météo.sh
@@ -12,20 +12,19 @@ ville=$1
 # Récupération des données brutes  depuis wttr.in
 
 data=$(curl -s "wttr.in/$ville?format=%C+%t") # %C pour météo et %t pour température
-echo "Données brutes : $data"
 
 
 # Vérifier si la récupération a reussi
-if [ $? -ne 0 ] || [ -z "$data" ]; then
+if [ $? -ne 0 ]; then
     echo "Erreur lors de la récupération des données météo."
     exit 1
 fi
 
 # Vérifier si les données récupérées contiennent une erreur  
-if [[ "$data" == *"404"* ]] || [[ "$data" == *"not found"* ]]; then
+if [[ "$data" == *"404"* ]] || [[ "$data" == *"not found"* ]] || [[ -z "$data" ]]; then
     echo "Erreur : Impossible de récupérer les données météo pour '$ville'. Ville introuvable."
-    echo 1
-
+    exit 1
+fi
 # Etape 2 : Extraire la température actuelle
 temperature_actuelle=$(echo "$data" |  awk '{print $2}')
 condition=$(echo "$data" | awk '{print $1}')
@@ -34,9 +33,16 @@ condition=$(echo "$data" | awk '{print $1}')
 # Pour obtenir la provision , nous ferons une autre requete
 prevision=$(curl -s "wttr.in/$ville?format=%t")
 
+# Vérifier si la prévision est vide 
+if [[ -z "$prevision" ]]; then
+    echo "Erreur : Impossible de récupérer la prévision météo pour '$ville'."
+    exit 1
+fi
+
+
 # Etape 4 : Enregistrer  les informations dans le fichier meteo.txt
-echo "$(date '+%Y-%m-%d %H:%M') -$ville : $temperature_actuelle - $prevision" >> meteo.txt
+echo "$(date '+%Y-%m-%d- %H:%M') - $ville : $temperature_actuelle - $prevision" >> meteo.txt
 
 # Affichage d'un message de confirmation 
-echo "Données météo enregistrées dans meteo.txt :"
+echo "Données météo enregistrées dans meteo.txt. allez-y dans le fichier pour les voir ."
 
